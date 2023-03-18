9ceaacea782002193b8f958ef563cd6d683db396
pedro <psalinas18@alumnos.utalca.cl>
2023-03-18 15:45:16 -0300

    Modelos predictivos GNN con db PIP usando descriptores y metodos de encoding

---
+++ edge_regression_pip.py
@@ -0,0 +1,136 @@
+import sys
+import os
+import torch
+import dgl
+import random
+import pandas as pd
+from plot import Plot
+import torch.nn as nn
+import torch.optim as optim
+from getData import getData
+from model_pip import EdgeWeightPredictor
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def main(methods):
+    all_losses = []
+    all_stages = []
+    all_methods = []
+    for method in methods:
+        data = getData()
+        if method == '':
+            method = 'seq_to_seq'
+            name = 'DB: PIP y features ' + method
+            g = data.seq_to_seq_edge_regression()
+        else:
+            name = 'DB: PIP y features ' + method
+            g = data.encoding_edge_regression(method)
+        num_features = g.ndata['feat'][0].shape[0]
+        num_edges = g.number_of_edges()
+
+        print("Utilizando metodo ", method)
+
+        n = int(num_edges * 0.8) # Primera division  
+        print("Se utilizara solo " + str(n) + " aristas en vez de las " + str(num_edges) + " del grafo")
+        ids_edges = list(range(num_edges))
+
+        # Especificar el tamaño de la muestra de prueba
+        tamano_val = int(n * 0.4) # Aquí se define la division entre el set de entrenamiento y validacion, (20% - 60%)
+        tamano_test = int(n * 0.2) # Aquí se reserva un 20% de la muestra para la validacion
+        # Dividir aleatoriamente la lista de edges
+        random.seed(123)
+        random.shuffle(ids_edges)
+        set_entrenamiento = ids_edges[tamano_val:n]
+        set_val = ids_edges[tamano_test:tamano_val]
+        set_test = ids_edges[:tamano_test]
+
+        sub_g = dgl.edge_subgraph(g, set_entrenamiento)
+        sub_g_val = dgl.edge_subgraph(g, set_val)
+        sub_g_test = dgl.edge_subgraph(g, set_test)
+
+        # SET ENTRENAMIENTO
+        src, dst = sub_g.edges()
+        # concatenar los tensores a lo largo de la dimensión 0
+        train_edges = torch.cat([src.unsqueeze(0), dst.unsqueeze(0)], dim=0)
+
+        # SET VALIDACION
+        src, dst = sub_g_val.edges()
+        # concatenar los tensores a lo largo de la dimensión 0
+        val_edges = torch.cat([src.unsqueeze(0), dst.unsqueeze(0)], dim=0)
+
+        # SET TEST
+        src, dst = sub_g_test.edges()
+        # concatenar los tensores a lo largo de la dimensión 0
+        test_edges = torch.cat([src.unsqueeze(0), dst.unsqueeze(0)], dim=0)
+
+        model = EdgeWeightPredictor(num_features, 1, 0.01)
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+        train_loss = []
+        val_loss = []
+        # Entrenar el modelo
+        num_epochs = 200
+        for epoch in range(num_epochs):
+            model.train()
+            optimizer.zero_grad()
+            out = model(sub_g.ndata['feat'], train_edges)
+            loss = criterion(out, sub_g.edata['weight'])
+            loss.backward()
+            optimizer.step()
+            # print(f"-----------------------------------------Epoch {epoch}") 
+            # print(f"Train loss: {loss.item():.4f}")
+            train_loss.append(loss.item())
+            if epoch == num_epochs - 1:
+                all_losses.append(loss.item())
+                all_stages.append('train')
+                all_methods.append(method)
+            with torch.no_grad():
+                model.eval()
+                out = model(sub_g_val.ndata['feat'], val_edges)
+                loss = criterion(out, sub_g_val.edata['weight'])
+                # print(f"Validation loss: {loss.item():.4f}")
+                val_loss.append(loss.item())
+                if epoch == num_epochs - 1:
+                    all_losses.append(loss.item())
+                    all_stages.append('validate')
+                    all_methods.append(method)
+        # Testeo
+        with torch.no_grad():
+            model.eval()
+            out = model(sub_g_test.ndata['feat'], test_edges)
+            loss = criterion(out, sub_g_test.edata['weight'])
+            # print('------------------TEST--------------------')
+            # print(f"Test Loss: {loss.item():.4f}")
+            # print("Valor minimo de true edge label: ", sub_g_test.edata['weight'].min())
+            # print("Valor maximo de true edge label: ", sub_g_test.edata['weight'].max())
+            # print("True edge label: ", sub_g_test.edata['weight'])
+            # print("Pred edge label: ", out)
+            all_losses.append(loss.item())
+            all_stages.append('test')
+            all_methods.append(method)
+        # Ploter
+        plot = Plot()
+        plot.graficar_loss_no_cutoff(train_loss, val_loss, name, method, 'edge_regression', num_epochs)
+        # Guardar modelo
+        # print("Entrenamiento termiando y modelo guardandose")
+        # torch.save(model, 'modelos/trainned_model_edge_regression_pip.pth')
+    tabla = pd.DataFrame()
+    tabla['Caracteristicas'] = all_methods
+    tabla['Etapa'] = all_stages
+    tabla['Perdida(MSE)'] = all_losses
+    tabla.to_csv('tables/pip_edge_regression_table' + str(num_epochs)  + '.csv', index=False)
+
+if __name__ == "__main__":
+    # if len(sys.argv) < 2:
+    #     methods = ['']
+    # else:
+    #     methods = [sys.argv[1]]
+    ruta = 'Protein-Protein/encoded_sequences'
+    methods = ['']
+    for elemento in os.listdir(ruta):
+        if os.path.isdir(os.path.join(ruta, elemento)):
+            methods.append(elemento)
+
+    main(methods)
---
+++ model_pip.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+from torch_geometric.nn import GCNConv
+
+
+class EdgeWeightPredictor(nn.Module):
+    def __init__(self, in_channels, out_channels, dropout_rate):
+        super(EdgeWeightPredictor, self).__init__()
+        self.g_conv1 = GCNConv(in_channels * 2, in_channels)
+        self.bn1 = nn.BatchNorm1d(in_channels)
+        self.g_conv2 = GCNConv(in_channels, in_channels)
+        self.ln2 = nn.LayerNorm(in_channels)
+        self.relu = nn.ReLU()
+        self.linear = nn.Linear(in_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        edge_features = torch.cat([x[edge_index[0]], x[edge_index[1]]], dim=1)
+        edge_scores = self.g_conv1(edge_features, edge_index)
+        edge_scores = self.bn1(edge_scores)
+        edge_scores = self.relu(edge_scores)
+        edge_scores = self.g_conv2(edge_scores, edge_index)
+        edge_scores = self.ln2(edge_scores)
+        edge_scores = self.relu(edge_scores)
+        edge_scores = self.linear(edge_scores)
+        # edge_scores = edge_scores - edge_scores.min()
+        
+        return edge_scores.squeeze()
+
+
+class Net(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super().__init__()
+        self.g_conv1 = GCNConv(in_channels, hidden_channels)
+        self.bn1 = nn.BatchNorm1d(hidden_channels)
+        self.g_conv2 = GCNConv(hidden_channels, out_channels)
+        self.ln2 = nn.LayerNorm(out_channels)
+        self.relu = nn.ReLU()
+        self.linear = nn.Linear(out_channels, out_channels)
+
+    def encode(self, x, edge_index):
+        x = self.g_conv1(x, edge_index)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.g_conv2(x, edge_index)
+        x = self.ln2(x)
+        x = self.relu(x)
+        x = self.linear(x)
+
+        return x
+
+    def decode(self, z, edge_label_index, cutoff):
+        edge_values = (z[edge_label_index[0].to(torch.long)] * z[edge_label_index[1].to(torch.long)]).sum(dim=-1)
+        rounded_values = torch.where(edge_values < cutoff, torch.zeros_like(edge_values), torch.ones_like(edge_values))
+
+        return edge_values.squeeze(), rounded_values.squeeze().detach()
